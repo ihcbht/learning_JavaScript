7d543441c1ebea88f9ffc4c1e5ad36f188cd4ae0
Ashutosh Das <123106325+ihcbht@users.noreply.github.com>
2025-03-11 08:36:47 +0000

    datatype summary

--- 1.Basics/datatypes.js
+++ 1.Basics/datatypes.js
@@ -31,4 +31,26 @@ console.log(typeof null);
 console.log(typeof undefined);
 console.log(typeof age);
 
+// String, Number, Boolean, null, undefined, Symbol, BigInt (Primitive)
+// (Reference) : Array, Objects, Functions
 
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null  // typeof(outsideTemp) : object
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log( id === anotherId) //false
+
+const bigNumber = 123456789852n //BigInt typeof(bigNumber): undefined
+
+const heroes = ["IronMan" , "Thor" , "Hulk"]
+
+let myobj = {
+    name: "Ashu",
+    age: 23,
+}
+
+const myfunction = function(){ //typeof(myfunction) : function
+    console.log("hello guys")
+}
