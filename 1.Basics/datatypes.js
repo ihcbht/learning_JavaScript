d9634d06d683b95ffa5be71f91f411d673517f34
Ashutosh Das <123106325+ihcbht@users.noreply.github.com>
2025-03-11 06:39:01 +0000

    datatypes

---
+++ 1.Basics/datatypes.js
@@ -0,0 +1,34 @@
+"use strict" //treat all js code as newer version
+
+//alert("hello") -- using node,js not browser
+
+// ensure code readability
+console.log(3
+    +3
+);
+
+//for documentation-- tc39.es, mdn
+
+let name = "ashu"
+let age = 25
+let osLoggedIn = false
+let state
+
+// number : 2 to the power of 53
+// bigint
+// string : "",'
+// boolean : true/false
+// null : standalone value (not undefined)
+// undefined : (no value given)
+// symbol : unique
+
+
+//object
+
+
+console.log(typeof "ashu")
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof age);
+
+
