0b449bbb8fbbc37756091b95493c37764b6f2dce
Ashutosh Das <123106325+ihcbht@users.noreply.github.com>
2025-03-11 06:39:21 +0000

    conversion

---
+++ 1.Basics/conversion_operations.js
@@ -0,0 +1,57 @@
+let score = 33
+let scores="33"
+
+console.log(typeof score);
+console.log(typeof (scores));
+
+
+let valueInNumber = Number(scores)
+console.log(typeof valueInNumber);
+
+let scoress="33aaa"
+console.log(typeof (scoress));
+let valueInNumberr = Number(scoress)
+console.log(typeof valueInNumberr);
+console.log(valueInNumberr);
+
+let scoresss=null
+console.log(typeof (scoresss));
+let valueInNumberrr = Number(scoresss)
+console.log(typeof valueInNumberrr);
+console.log(valueInNumberrr);
+
+let scoressss=undefined
+console.log(typeof (scoressss));
+let valueInNumberrrr = Number(scoressss)
+console.log(typeof valueInNumberrrr);
+console.log(valueInNumberrrr);
+
+let scoresssss=false
+console.log(typeof (scoresssss));
+let valueInNumberrrrr = Number(scoresssss)
+console.log(typeof valueInNumberrrrr);
+console.log(valueInNumberrrrr);
+
+
+// "33" -> 33
+// "33aaa" -> NaN
+// true -> 1, false-> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let isLoggedInn = ""
+let booleanIsLoggedInn = Boolean(isLoggedInn)
+console.log(booleanIsLoggedInn);
+
+// 1-> true, 0->false
+//""-> false
+// "ashu"->true
+
+let someNumber =33
+let stringNumber=String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber);
+
+
