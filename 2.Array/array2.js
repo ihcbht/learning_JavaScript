69cf59c7e635209490ead8d3ad45d4a87d4eba26
Ashutosh Das <123106325+ihcbht@users.noreply.github.com>
2025-03-12 05:19:26 +0000

    array2

---
+++ 2.Array/array2.js
@@ -0,0 +1,39 @@
+const marvel_heroes = ["thor", "IronMan", "Hulk"]
+const dc_heroes = ["Superman", "Flash", "Batman"]
+
+//marvel_heroes.push(dc_heroes)
+//console.log(marvel_heroes);
+// [ 'thor', 'IronMan', 'Hulk', [ 'Superman', 'Flash', 'Batman' ] ]
+
+const all_heroes = marvel_heroes.concat(dc_heroes)
+console.log(all_heroes);
+// [ 'thor', 'IronMan', 'Hulk', 'Superman', 'Flash', 'Batman' ]
+
+const all_newHeroes = [...marvel_heroes, ...dc_heroes]
+console.log(all_newHeroes);
+//[ 'thor', 'IronMan', 'Hulk', 'Superman', 'Flash', 'Batman' ]
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+// [
+//     1, 2, 3, 4, 5,
+//     6, 7, 6, 7, 4,
+//     5
+//   ]
+
+console.log(Array.isArray("Ashutosh")); //false
+console.log(Array.from("Ashutosh"));
+// [
+//     'A', 's', 'h',
+//     'u', 't', 'o',
+//     's', 'h'
+//   ]
+
+console.log(Array.from({name:"Ashu"})); //[]
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1, score2, score3)); //[ 100, 200, 300 ]
+
